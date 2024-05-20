3a4fcf13a1800bbde482f3045b77ce61470381e6
PieMenard <110061246+PieMenard@users.noreply.github.com>
2024-05-20 11:42:03 -0400

    create H1 component and add scrollbar

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -1,5 +1,23 @@
-const Events = () => {
-  return <div>Events Page</div>;
+import H1 from '@/components/h1';
+
+type EventspageProps = {
+  params: {
+    city: string;
+  };
 };
 
-export default Events;
+const EventsPage = ({ params }: EventspageProps) => {
+  const city = params.city;
+
+  return (
+    <main className="flex flex-col items-center py-24 px-[20px] min-h-[110vh]">
+      <H1>
+        {city === 'all' && 'All Events'}
+        {city !== 'all' &&
+          `Events in ${city.charAt(0).toUpperCase() + city.slice(1)}`}
+      </H1>
+    </main>
+  );
+};
+
+export default EventsPage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,17 @@
 import SearchForm from '@/components/SearchForm';
+import H1 from '@/components/h1';
 import Link from 'next/link';
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center px-3 pt-36">
-      <h1 className="text-3xl lg:text-6xl font-bold tracking-tight">
-        Find events around you
-      </h1>
+      <H1>Find events around you </H1>
+
       <p className="mb-12 mt-7 text-2xl lg:text-3xl opacity-75">
         Browse more than
         <span className="font-bold text-accent italic underline">
           10,000 events
-        </span>
+        </span>{' '}
         around you
       </p>
 
