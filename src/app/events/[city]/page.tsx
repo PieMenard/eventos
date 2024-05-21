0e5e032d4535039d9c1e59799f685bc53a3214bd
PieMenard <110061246+PieMenard@users.noreply.github.com>
2024-05-21 15:54:16 -0400

    fix loading blocking

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -1,21 +1,16 @@
+import Loading from './loading';
 import EventsList from '@/components/events-list';
 import H1 from '@/components/h1';
-import { EventosEvent } from '@/lib/types';
+import { Suspense } from 'react';
 
-type EventspageProps = {
+type EventsPageProps = {
   params: {
     city: string;
   };
 };
 
-const EventsPage = async ({ params }: EventspageProps) => {
+const EventsPage = async ({ params }: EventsPageProps) => {
   const city = params.city;
-
-  const response = await fetch(
-    `https://bytegrad.com/course-assets/projects/evento/api/events?city=${city}`
-  );
-
-  const events: EventosEvent[] = await response.json();
 
   return (
     <main className="flex flex-col items-center py-24 px-[20px] min-h-[110vh]">
@@ -25,7 +20,9 @@ const EventsPage = async ({ params }: EventspageProps) => {
           `Events in ${city.charAt(0).toUpperCase() + city.slice(1)}`}
       </H1>
 
-      <EventsList events={events} />
+      <Suspense fallback={<Loading />}>
+        <EventsList city={city} />
+      </Suspense>
     </main>
   );
 };
--- src/components/events-list.tsx
+++ src/components/events-list.tsx
@@ -2,10 +2,16 @@ import { EventosEvent } from '@/lib/types';
 import EventCard from './event-card';
 
 type EventsListProps = {
-  events: EventosEvent[];
+  city: string;
 };
 
-const EventsList = ({ events }: EventsListProps) => {
+const EventsList = async ({ city }: EventsListProps) => {
+  const response = await fetch(
+    `https://bytegrad.com/course-assets/projects/evento/api/events?city=${city}`
+  );
+
+  const events: EventosEvent[] = await response.json();
+
   return (
     <section className="max-w-[1100px] flex flex-wrap gap-10 justify-center px-[20x]">
       {events.map((event) => (
