7e991eb1ab541a1ddf577af5d8ebd9976cba8714
PieMenard <110061246+PieMenard@users.noreply.github.com>
2024-05-22 10:24:29 -0400

    add event card scroll based animation

--- src/components/event-card.tsx
+++ src/components/event-card.tsx
@@ -1,16 +1,41 @@
+'use client';
+
 import { EventosEvent } from '@/lib/types';
+import { motion, useScroll, useTransform } from 'framer-motion';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useRef } from 'react';
 
 type EventCardProps = {
   event: EventosEvent;
 };
 
+const MotionLink = motion(Link);
+
 const EventCard = ({ event }: EventCardProps) => {
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ['0 1', '1.5 1'],
+  });
+  const scaleProgress = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
+  const opacityProgress = useTransform(scrollYProgress, [0, 1], [0.3, 1]);
+
   return (
-    <Link
+    <MotionLink
+      ref={ref}
       className="flex-1 basis-80 h-[380px] max-w-[500px]"
       href={`/event/${event.slug}`}
+      style={{
+        // @ts-ignore
+        scale: scaleProgress,
+        // @ts-ignore
+        opacity: opacityProgress,
+      }}
+      initial={{
+        opacity: 0,
+        scale: 0.8,
+      }}
     >
       <section
         className="w-full h-full flex flex-col bg-white/[3%] rounded/xl overflow-hidden relative 
@@ -41,7 +66,7 @@ const EventCard = ({ event }: EventCardProps) => {
           </p>
         </section>
       </section>
-    </Link>
+    </MotionLink>
   );
 };
 
