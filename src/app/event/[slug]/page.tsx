6b766d2a6ed5c31e77e43328caa3f45d2f0fa670
PieMenard <pinedo.m.gustavo@gmail.com>
2024-05-22 00:07:39 -0400

    displaye vent name as title/data fetching utilities

--- src/app/event/[slug]/page.tsx
+++ src/app/event/[slug]/page.tsx
@@ -1,18 +1,26 @@
 import H1 from '@/components/h1';
+import { capitalize, getEvent } from '@/lib/utils';
+import { Metadata } from 'next';
 import Image from 'next/image';
 
-type EventPageProps = {
+type Props = {
   params: {
     slug: string;
   };
 };
 
-const EventPage = async ({ params }: EventPageProps) => {
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
   const slug = params.slug;
-  const response = await fetch(
-    `https://bytegrad.com/course-assets/projects/evento/api/events/${slug}`
-  );
-  const event = await response.json();
+  const event = await getEvent(slug);
+
+  return {
+    title: event.name,
+  };
+}
+
+const EventPage = async ({ params }: Props) => {
+  const slug = params.slug;
+  const event = await getEvent(slug);
 
   return (
     <main>
