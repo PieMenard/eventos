c7dcb48c558f228d700fd17a8482e49f3e2a139d
PieMenard <110061246+PieMenard@users.noreply.github.com>
2024-05-23 15:12:40 -0400

    reimport components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+
 import Header from '@/components/header';
-import Footer from '@/components/footer';
 import Container from '@/components/container';
+import Footer from '@/components/footer';
 
 const inter = Inter({ subsets: ['latin'] });
 
